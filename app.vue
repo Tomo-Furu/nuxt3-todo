7ade5e46056187069634f1194b669e8f4d81645d
Tomo Furu <to.frkw@ymail.ne.jp>
2024-06-25 13:54:35 +0900

    Create add task function

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>Hello Nuxt3!!</h1>
+    <h1>ToDoApp</h1>
+    <input v-model="taskName">
+    <button @click="addTask()">Add</button>
+    <h2>Task List</h2>
+      <div v-for="taskName in taskNameList" :key="taskName">
+        {{ taskName }}
+      </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const taskNameList = ref<string[]>(['vue', 'Vue3', 'Nuxt3']);
+  const taskName = ref<string>('');
+  const addTask = () => {
+    if(taskName.value === '') {
+      return;
+    }
+    taskNameList.value.push(taskName.value);
+    taskName.value = '';
+  }
+</script>
